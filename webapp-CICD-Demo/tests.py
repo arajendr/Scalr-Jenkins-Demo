0430e58828ad765da91cb190efe3ec8024083b28
Ashok Rajendran <ashok.rajendran2@cognizant.com>
2018-09-25 22:43:49 +0530

    Signed-off-by: Ashok Rajendran <ashok.rajendran2@cognizant.com>

---
+++ webapp-CICD-Demo/tests.py
@@ -0,0 +1,19 @@
+import time
+import unittest
+
+
+class DemoTestCase(unittest.TestCase):
+    def test_first(self):
+        time.sleep(1)
+
+    def test_second(self):
+        time.sleep(1)
+
+    def test_third(self):
+        time.sleep(1)
+
+#    def test_failed(self):
+#        raise AssertionError('Test failed!')
+
+if __name__ == '__main__':
+    unittest.main()
